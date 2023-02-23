eec68d3deac5d4e55ca3a9f7914d58a6f18937ee
jammykii <sdh210406@sdh.hs.kr>
2023-02-23 11:05:47 +0900

    Update news_crowl.py

--- crowl_json/news_crowl.py
+++ crowl_json/news_crowl.py
@@ -25,16 +25,26 @@ def news_crowl(value):
     url = "http://www.newsmp.com/news/articleList.html?sc_section_code=S1N2&view_type=sm"
     driver.get(url)
     search_tag = driver.find_element(By.ID, "search")
+    newslist = []
+    data_news = {}
+    count = 0
     if value is None:
-        value = " "
+        thnlist = driver.find_elements(By.CLASS_NAME, "list-block")
+        for i in thnlist:
+            count += 1
+            rank = count
+            title = re.sub('(<([^>]+)>)', '', str(i.find_element(By.TAG_NAME, "strong").text)).replace("\n", "")
+            summary = re.sub('(<([^>]+)>)', '', str(i.find_element(By.CLASS_NAME, "line-height-3-2x").text)).replace("\n", "").replace(" &amp;", "").replace("\t", "").replace("[의약뉴스]", "").replace("]", "").replace("[","")
+            date = re.sub('(<([^>]+)>)', '', str(i.find_element(By.CLASS_NAME, "list-dated").text)).replace("\n","").replace("[", "").replace("]", "")
+            newslist.append([rank, title, summary, date])
+            data_news[str(rank)] = {'title': title, 'summary': summary, 'date': date}
+        json_val = json.dumps(data_news)
+        return json_val
     decordeValue = parse.unquote(value)
     search_tag.send_keys(decordeValue)
     search_tag.send_keys(Keys.ENTER)
     time.sleep(1)
     thnlist = driver.find_elements(By.CLASS_NAME, "list-block")
-    newslist = []
-    data_news = {}
-    count = 0
     # list2 = thnlist.find_all(".item")
     for i in thnlist:
         count += 1

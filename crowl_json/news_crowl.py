f4b1a312424315f2313b195c1f5691299b5868f3
jammykii <sdh210406@sdh.hs.kr>
2023-03-21 19:54:19 +0900

    Update news_crowl.py

--- crowl_json/news_crowl.py
+++ crowl_json/news_crowl.py
@@ -47,17 +47,22 @@ def request_url(value):
             html = request.text
             soup = BeautifulSoup(html, 'html.parser')
             thnlist = soup.select(".list-block")
-            for i in thnlist:
-                if decordeValue in i.select_one("strong").text:
-                    count += 1
-                    title = re.sub('(<([^>]+)>)', '', str(i.select_one("strong").text)).replace("\n", "")
-                    summary = re.sub('(<([^>]+)>)', '', str(i.select_one(".line-height-3-2x").text)).replace("\n", "").replace(" &amp;", "").replace("\t", "").replace("[의약뉴스]", "").replace("]", "").replace("[", "")
-                    date = re.sub('(<([^>]+)>)', '', str(i.select_one(".list-dated").text)).replace("\n", "").replace("[","").replace("]", "")
-                    link = "http://www.newsmp.com"
-                    link += re.sub('(<([^>]+)>)', '', str(i.select_one("a").get_attribute_list("href"))).replace("['","").replace("']", "")
-                    newslist.append([title, summary, link, date])
-                    data_news = {'title': title, 'summary': summary, 'link': link, 'date': date}
-                    dataJson.append(data_news)
+            if len(thnlist) == 0:
+                data_news['title'] = '검색결과가 없습니다.'
+                dataJson.append(data_news)
+                break
+            else:
+                for i in thnlist:
+                    if decordeValue in i.select_one("strong").text:
+                        count += 1
+                        title = re.sub('(<([^>]+)>)', '', str(i.select_one("strong").text)).replace("\n", "")
+                        summary = re.sub('(<([^>]+)>)', '', str(i.select_one(".line-height-3-2x").text)).replace("\n", "").replace(" &amp;", "").replace("\t", "").replace("[의약뉴스]", "").replace("]", "").replace("[", "")
+                        date = re.sub('(<([^>]+)>)', '', str(i.select_one(".list-dated").text)).replace("\n", "").replace("[","").replace("]", "")
+                        link = "http://www.newsmp.com"
+                        link += re.sub('(<([^>]+)>)', '', str(i.select_one("a").get_attribute_list("href"))).replace("['","").replace("']", "")
+                        newslist.append([title, summary, link, date])
+                        data_news = {'title': title, 'summary': summary, 'link': link, 'date': date}
+                        dataJson.append(data_news)
         return dataJson
 
 def news_crowl(value):
